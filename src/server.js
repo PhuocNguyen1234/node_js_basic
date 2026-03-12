5dfe9cb4994ad9b239e6f233ad7e5628fddc73fd
nguyenphuoc0768 <nguyenphuoc0768@gmail.com>
2026-03-12 22:21:56 +0700

    Ket noi database buoc dau

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 require('dotenv').config();
 const configViewEngine = require('./config/viewEngine');
-const webRoutes = require('./routes/web')
+const webRoutes = require('./routes/web');
+const mysql = require('mysql2');
 
 const app = express();
 const port = process.env.PORT || 8888;
@@ -12,6 +13,24 @@ configViewEngine(app);
 
 //Khai bao route
 app.use('/', webRoutes);
+
+//Test connect DB
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3307, //Default 3306
+  user: 'root',
+  password: '123456',
+  database: 'huuphuoc'
+})
+
+// A simple SELECT query
+  connection.query(
+    'Select *from Users u',
+    function (err, results, fields) {
+      console.log('>>Results: ',results); // results contains rows returned by server
+      console.log('>>Fields: ',fields); // fields contains extra meta data about results, if available
+    }
+  ); 
 
 app.listen(port, hostname, () => {
   console.log(`Example app listening on port ${port}`);
